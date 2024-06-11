c0dcb3e541c71868b6ae3fff341b6bda7d772fe0
aga <vl-mr_123@mail.ru>
2024-06-11 18:48:28 +0300

    Добавлен тестовый исполняемый файл для анализа графиков.
    Работа файла состоит в получении конфигурации анализа тикера,
    расположенного в папке configs, извлечения всех торговых стратегий
    для данного тикера, формирование цепочки модулей для анализа
    графиков и вызов у всех модулей из цепочки метода decide, который
    вернет торговое решение по выбранному тикеру.
    
    *** Дописать логику сохранения торговых решений в базу данных ***

---
+++ analize.py
@@ -0,0 +1,66 @@
+from storages import CandleStorage
+from importlib import import_module
+import pandas as pd
+import os, json
+
+def load_modules(mdir):
+    modules = {}
+    for modulefile in os.listdir(mdir):
+        try:
+            module_name = mdir.replace('./', '') + \
+                    modulefile.replace('.py', '') 
+            module_name = module_name.replace('/', '.')
+            class_name = modulefile.replace('.py', '') 
+
+            module = import_module(module_name)
+            modules[class_name] = getattr(module, class_name)
+        except:
+            print(f'Error import module {module_name}') 
+    return modules
+
+def load_configs(cdir):
+    config = {}
+    for conffile in os.listdir(cdir):
+        with open(cdir + conffile, 'r' ) as f:
+            conf = eval(f.read())
+            ticker_name = conf['ticker_name']
+            del conf['ticker_name']
+            config[ticker_name] = conf
+    return config
+
+def get_all_ticker_candles(ticker, tfs):
+    candles = {}
+    for tf in tfs:
+        candles[tf] = db.get_all_candles(ticker, tf)
+    return candles
+
+def get_modules_chain(conf):
+    chain = []
+    for module in conf['modules_order']:
+        if module not in modules:
+            continue
+        chain.append(modules[module](conf['modules_conf'][module]))
+    return chain
+
+def save_solution(trade_solution, ticker, strategy):
+    if ticker not in trade_solutions:
+        trade_solutions[ticker] = {}
+    if strategy not in trade_solutions[ticker]:
+        trade_solutions[ticker][strategy] = {}
+    trade_solutions[ticker][strategy] = trade_solution
+
+modules = load_modules('./modules/')
+db = CandleStorage('185.58.191.251', 9000)
+conf = load_configs('./configs/')
+tickers = conf.keys()
+trade_solutions = {}
+
+for tik in tickers:
+    tc = conf[tik]
+    candles = get_all_ticker_candles(tik, tc['required_timeframes']) 
+
+    for strategy in tc['trade_strategies']:
+        modules_chain = get_modules_chain(tc['trade_strategies'][strategy])
+        for module in modules_chain:
+            solution, candles  = module.decide(candles)
+            save_solution(solution, tik, strategy)
