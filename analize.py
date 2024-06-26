475f3f83ea29904fc8236ad675a2dac3b5fae389
aga <vl-mr_123@mail.ru>
2024-06-27 02:13:25 +0300

    Добавлена стремная логика работы стратегий совместно с файлами конфигов
    В файле configs/tickers/... Лежит конфиг, согласно которому
    	биткоину нужны почти все тф для работы, биток по
    	работает со стандартной стратегией TrandPlusRSI и
    	имеет кастомную стратегию TrandPlusRSI, в которой
    	переопределены таймфреймы (m30 m5 всесто D H4)
    
    В файле configs/strategies/... Лежит конфиг. Это стандартный конфиг
    стратегии, по которому эта стратегия сначала определяет тренды на
    дневке и ч4 и потом определяет RSI на дневке и 4ч.

--- analize.py
+++ analize.py
@@ -1,45 +1,17 @@
 from storages import CandleStorage
+from importlib import import_module
+import os
 
-tickers = ['1000PEPEPERP', 'BTCUSDT']
-tfs = ["M1", "M5", "M30", "H1", "H4", "D"]
-ip, port = ('38.180.68.156', 9000)
-ticker_cfg = {
-    '1000PEPEPERP' : {
-        'use_tfs' : ['H1'],
-        'trade_strategies' : {
-            'default' : ['RSI'],
-            'custom' : [
-                {
-                    'base' : 'RSI',
-					'modules_conf' : {
-						'RSI' : {
-							'use_tf' : ['H4']
-						}
-					}
-                }
-            ]
-        }
-     },
-
-    'BTCUSDT' : {
-        'use_tfs' : ['H1'],
-        'trade_strategies' : {
-            'default' : ['RSI'],
-            'custom' : [
-                {
-                    'base' : 'RSI',
-					'modules_conf' : {
-						'RSI' : {
-							'use_tf' : ['H4']
-						}
-					}
-                }
-            ]
-        }
-     },
+pathes = {
+    'tickers_configs' : 'configs/tickers/',
+    'default_strategies_configs' : 'configs/strategies/',
+    'strategies' : 'strategies/',
+    'modules' : 'modules/'
 }
 
-db = CandleStorage(ip, port)
+def load_conf(file):
+    with open(file, 'r')  as f:
+        return eval(f.read())
 
 def get_all_ticker_candles(ticker, tfs):
     candles = {}
@@ -47,35 +19,36 @@ def get_all_ticker_candles(ticker, tfs):
         candles[tf] = db.get_all_candles(ticker, tf)
     return candles
 
-conf = {
-    'Pathes' : {
-        'Strategies' : './strategies/'
-    }
-}
-
 def load_strategy(strat):
-    strat_path = conf['Pathes']['Strategies'] 
-    full_path = strat_path + strat + '/'
-    return None
+    strat_dir = pathes['strategies']
+    strat_conf_dir = pathes['default_strategies_configs']
+    strat_cfg = load_conf(strat_conf_dir + strat + '.json')
+    mod = strat_dir.replace('/', '.') + strat
+    module = import_module(mod)
+    class_ = getattr(module, strat)
+    return class_(strat_cfg)
 
+def load_tickers_configs():
+    path = pathes['tickers_configs']
+    confs = {}
+    for filename in os.listdir(path):
+        ticker_name = filename.replace('.json', '')
+        confs[ticker_name] = load_conf(path + filename)
+    return confs
 
-for tick in tickers:
-    tick_conf = ticker_cfg[tick]
+db = CandleStorage('38.180.68.156', 9000)
+conf = load_tickers_configs()
+
+for tick in conf.keys():
+    tick_conf = conf[tick]
+
     # получили требуемые для анализа свечки
-    big_dic = get_all_ticker_candles(tick, tick_conf['use_tfs'])
-    for c in big_dic['H1']:
-        print(c)
+    big_dic = get_all_ticker_candles(tick, tick_conf['required_timeframes'])
 
     # загружаем стандартные стратегии (которые по умолчанию)
-    # анализируем свечки тикера ими каждой из стратегий
+    # анализируем свечки тикера каждой из стратегий
     for default in tick_conf['trade_strategies']['default']:
         strategy = load_strategy(default)
-
-
-
-
-
-
-
+        print(default, tick, strategy.decide(big_dic)[0])
 
 
