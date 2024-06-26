e53a5237e97fa7618501d1e83e4017fc6bd9cdf1
aga <vl-mr_123@mail.ru>
2024-06-27 01:13:52 +0300

    Добавлена стремная стратегия
    Стратегия работает с 2 модулями.
    1 модуль определяет тренды по старшему и младшему тф
    2 модуль определяет RSI на старшем и младшем тф
    Стратегия, имея результаты работы модулей, ищет паттерны на
    покупку или продажу:
    	1) На старшем тф тренд вверх
    	   На младшем тф тренд вниз
    	   RSI на старшем тф < 30 - покупка
    	2) На страшем - вниз
    	   На младшем - вверх
    	   RSI на старшем > 70 - шорт

---
+++ strategies/TrandPlusRSI.py
@@ -0,0 +1,41 @@
+from base import Strategy
+import pandas as pd
+
+class TrandPlusRSI(Strategy):
+
+    def is_trand_up(self, line_avg):
+        return line_avg[0] > line_avg[len(line_avg)-1]
+
+    def decide(self, data):
+
+        for trade_module in self.modules:
+            module_name = trade_module['name']
+            module_obj = trade_module['obj']
+            data = module_obj.analize(data)
+
+        try:
+            low_tf_candles = pd.DataFrame(data[self.conf['TrandDetermer']['tf_low']])
+            high_tf_candles = pd.DataFrame(data[self.conf['TrandDetermer']['tf_high']])
+
+            # если тренд старшего таймфрейма ебашит
+            # вверх то 100 проц ищем сделки на покупку
+            # на младшем таймфрейме, при условии, что
+            # тренд на младшем ебашит вниз и низкий rsi
+            if self.is_trand_up(high_tf_candles['AVG']):
+                if not self.is_trand_up(low_tf_candles['AVG']):
+                    if low_tf_candles['RSI'][len(low_tf_candles) -1] <= 30:
+                        self.sol['action'] = 'buy'
+
+            # если ебашит вниз, то все наоборот
+            elif not self.is_trand_up(high_tf_candles['AVG']):
+                if  self.is_trand_up(low_tf_candles['AVG']):
+                    if low_tf_candles['RSI'][len(low_tf_candles) - 1] >= 70:
+                        self.sol['action'] = 'sell'
+
+            # если не туда не туда - нахуй надo
+            if self.sol['action'] == None:
+                self.sol['action'] = 'keep'
+        except:
+            self.sol['log'] = 'error'
+
+        return (self.sol, data)
