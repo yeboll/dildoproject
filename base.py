b1508662687ee45c1095e183a9094b034d6e53e2
aga <vl-mr_123@mail.ru>
2024-06-26 22:38:18 +0300

    Добавлена базовая логика для стратегии
    Стратегия принимает в конструктор конфиг, в котором
    содержатся конфиги модулей, модули импортятся из папки
    modules, делается список модулей.
    Список модулей может использоваться в decide методе.

---
+++ base.py
@@ -0,0 +1,53 @@
+import os
+from importlib import import_module
+modules_path = 'modules/'
+
+class Strategy():
+
+    def load_strategy_modules(self, mod_cfg):
+        modules = []
+        for module_name in mod_cfg.keys():
+            try:
+                mod = modules_path + module_name
+                mod = mod.replace('/', '.')
+                module = import_module(mod)
+                class_ = getattr(module, module_name)
+                module_obj = class_(mod_cfg[module_name])
+                modules.append(module_obj)
+            except:
+                pass
+        return modules
+
+
+    # сюда передаем конфиги, и стратегия
+    # должна сама автоматически подгрузить
+    # необходимые модули для анализа свеч
+    def __init__(self, conf):
+        self.sol = {
+            'action' : None,
+            'tp' : None,
+            'sl' : None,
+            'deadline' : None,
+            'log' : None
+        }
+        self.modules = self.load_strategy_modules(conf)
+
+    # с использованием всех сконфигурированных
+    # модулей сформировать торговое решение и вернуть
+    # его в self.sol, в data - данные, на основе которых
+    # было принято это решение
+    def decide(self, data):
+        return (self.sol, data) 
+
+class Module():
+    def __init__(self, conf):
+        self.conf = conf
+
+    # в data добавляем необходимые колонки
+    # которые будут использоваться стратегией
+    # для формирования торгового решения
+    def analize(self, data):
+        return data
+
+
+
